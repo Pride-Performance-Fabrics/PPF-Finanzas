c6e1c0c12ddb122c1b38c81a02a9d14d2730e955
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2023-02-17 11:27:47 -0600

    Se agrego la funcion de cambiar el estado de la actividad en el modal

--- src/Api/Sheduler/ShedulerRequest.js
+++ src/Api/Sheduler/ShedulerRequest.js
@@ -29,7 +29,7 @@ export const updateScheduler = async(data) => {
  //***************************** ELIMINA UN EVENTOS  ***********************************//
  export const updateEstadoScheduler = async(id) => {
    console.log(id)
-    const respuesta = await PrivateInterceptor(`schedulerCalendar/${id}`, 'PUT')
+    const respuesta = await PrivateInterceptor(`schedulerCalendar/cancelarActividad/${id}`, 'PUT')
     return respuesta;
  }
 
@@ -37,4 +37,18 @@ export const updateScheduler = async(data) => {
    const respuesta = await PrivateInterceptor(`schedulerCalendar/notificaciones`, 'GET')
    return respuesta;
  }
+
+ export const putEstadoActividad = async(data) => {
+   console.log(data)
+   const respuesta = await PrivateInterceptor(`schedulerCalendar/cambiarEstado`, 'PUT', data)
+   return respuesta;
+ }
+
+ export const getActividad = async(id) => {
+   console.log(id)
+   const respuesta = await PrivateInterceptor(`schedulerCalendar/obtenerActividad/${id}`, 'GET')
+   return respuesta;
+ }
+
+
  
