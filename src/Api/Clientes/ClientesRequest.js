283d541cbcabe9cf3bdd5d3e79f8441346987e02
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2023-03-03 13:59:32 -0600

    Peticiones a la API Para la funcionalidad del modulo de clientes detalle

--- src/Api/Clientes/ClientesRequest.js
+++ src/Api/Clientes/ClientesRequest.js
@@ -9,17 +9,7 @@ export const getCustomers = async () => {
 }
 
 
-export const getALLCustomersDetails = async () => {
-    
-    const result = await PrivateInterceptor(`clientes/customerDetails`, 'GET');
-    return result;
 
-}
-
-export const getCustomerDetails = async(IdCustomer) =>{
-    const result = await PrivateInterceptor(`clientes/customerDetails/${IdCustomer}`, 'GET');
-    return result;
-}
 
 export const getUltimoIdCustomer = async() =>{
     const result = await PrivateInterceptor(`clientes/customerUltimoIdCustomer`, 'GET');
@@ -32,4 +22,27 @@ export const postCustomer = async(data) =>{
 
 }
 
+//************************************** CUSTOMER DETAILS ************************************** */
 
+export const getALLCustomersDetails = async () => {
+    
+    const result = await PrivateInterceptor(`clientes/customerDetails`, 'GET');
+    return result;
+
+}
+
+export const getCustomerDetails = async(IdCustomer) =>{
+    const result = await PrivateInterceptor(`clientes/customerDetails/${IdCustomer}`, 'GET');
+    return result;
+}
+
+export const getUltimoIdCustomerDetails = async() =>{
+    const result = await PrivateInterceptor(`clientes/customerDetailsUltimoId`, 'GET');
+    return result;
+}
+
+
+export const postCustomerDetails = async (data) =>{
+    const result = await PrivateInterceptor(`clientes/IUCustomerDetails`, 'POST', data);
+    return result;
+}
