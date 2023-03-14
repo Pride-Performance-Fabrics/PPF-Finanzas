629dfd665491185a365dfaa6325196be26cb9461
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2023-03-14 11:17:41 -0600

    Se agrego la API de IPS al sistemas de finanzas

--- src/Api/backend.js
+++ src/Api/backend.js
@@ -2,7 +2,8 @@ const instancias = (
     {
         API_URL: process.env.REACT_APP_API_URL,
         API_URLPR: process.env.REACT_APP_API_URLPR,
-        API_PORT: process.env.REACT_APP_API_PORT
+        API_PORT: process.env.REACT_APP_API_PORT,
+        API_URL_IPS: process.env.REACT_APP_API_URL_IPS
     }
 )
 
